ec85c250d49823950b80065d02a0f8d37b665adf
angel-py349 <jula040513@gmail.com>
2025-02-23 22:13:28 -0600

    oña

---
+++ movieManager/prueba/pruebapage.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace movieManager.prueba;
+
+public partial class pruebapage : Page
+{
+    public pruebapage()
+    {
+        InitializeComponent();
+    }
+}
